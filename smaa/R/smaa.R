485148424eb7ab7341a9797373eb2a5d10b63d79
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-02-18 16:23:57 +0100

    Add man page

--- smaa/R/smaa.R
+++ smaa/R/smaa.R
@@ -1,13 +1,7 @@
-# Calculate SMAA rank acceptability indices and central weights
-# N: no. of iterations (default 10,000)
-# m: no. of alternatives
-# n: no. of criteria
-# meas: criteria measurements: N.m.n array, where meas[i,,] is a matrix where
-# the m alternatives are the rows, the n criteria the columns. The values must
-# be *standardized* measurements (i.e. after application of the partial value
-# function).
-# pref: weights: N.n array, where pref[i,] is a (normalized) weight vector
-smaa <- function(m, n, meas, pref, N=10000) {
+smaa <- function(meas, pref, m=dim(meas)[2], n=dim(meas)[3], N=dim(meas)[1]) {
+	stopifnot(identical(dim(meas), c(N, m, n)))
+	stopifnot(identical(dim(pref), c(N, n)))
+
 	result <- .C("smaa", as.double(aperm(meas, c(2,3,1))), as.double(t(pref)),
 		as.integer(N), as.integer(m), as.integer(n),
 		h=matrix(0.0, nrow=m, ncol=m),
