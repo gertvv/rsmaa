a810cc1bd690b5041a025141a3bd246b496d3bb6
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-02-18 16:10:18 +0100

    Remove redundant code

--- smaa/R/smaa.R
+++ smaa/R/smaa.R
@@ -8,32 +8,6 @@
 # function).
 # pref: weights: N.n array, where pref[i,] is a (normalized) weight vector
 smaa <- function(m, n, meas, pref, N=10000) {
-	h <- matrix(0, nrow=m, ncol=m) # rank frequency table 
-	wc <- matrix(0, nrow=m, ncol=n) # central weights
-	for (k in 1:N) {
-		w <- pref[k,]
-		x <- meas[k,,]
-		t <- x %*% w # calculate additive linear value function
-
-		r <- rank(-t, ties.method='min') # rank alternatives
-		for (i in 1:m) {
-			h[i, r[i]] <- h[i, r[i]] + 1 # update rank counts
-			if (r[i] == 1) {
-				wc[i,] <- wc[i,] + w # update central weights
-			}
-		}
-	}
-	for (i in 1:m) {
-		if (h[i, 1] > 0) {
-			wc[i,] <- wc[i,] / h[i, 1] # normalize central weight
-		}
-	}
-	list(rankAcc=h/N, centralWeights=wc)
-}
-
-#dyn.load("smaa.so")
-
-smaa <- function(m, n, meas, pref, N=10000) {
 	result <- .C("smaa", as.double(aperm(meas, c(2,3,1))), as.double(t(pref)),
 		as.integer(N), as.integer(m), as.integer(n),
 		h=matrix(0.0, nrow=m, ncol=m),
