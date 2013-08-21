ca30b7d534e89062e5a7ee2451c7d35d9f4fa06f
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-08-21 14:15:10 +0200

    Add test for outOfBounds="clip" in smaa.pvf

--- smaa/tests/pvf.R
+++ smaa/tests/pvf.R
@@ -44,3 +44,17 @@ pvf5 <- function(x) {
 
 stopifnot(all.equal(pvf5(c(50,90,100)), c(1.0, 0.5, 0.0)))
 stopifnot(all.equal(pvf5(c(10, 40, 101, 120)), c(1.32, 1.08, -0.05, -1)))
+
+pvf6 <- function(x) {
+  smaa.pvf(x, cutoffs=c(50, 75, 90, 100), values=c(1, 0.8, 0.5, 0), outOfBounds="clip")
+}
+
+stopifnot(all.equal(pvf6(c(50,90,100)), c(1.0, 0.5, 0.0)))
+stopifnot(all.equal(pvf6(c(10, 40, 101, 120)), c(1.0, 1.0, 0.0, 0.0)))
+
+pvf7 <- function(x) {
+  smaa.pvf(x, cutoffs=c(-0.15, 0.0, 0.25, 0.35), values=c(0, 0.1, 0.9, 1), outOfBounds="clip")
+}
+
+stopifnot(all.equal(pvf7(c(0.35, -0.15, 0.0, 0.25)), c(1.0, 0.0, 0.1, 0.9)))
+stopifnot(all.equal(pvf7(c(0.38, -0.20)), c(1.0, 0.0)))
